3d3e99f85d4f215057d8ed5e6131f904b31a5521
paulsin <paulsin91@gmail.com>
2023-12-29 16:30:17 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,10 @@
 from flask import Flask
-import threading
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World paulsin! ghgfh'
+    return 'Hello, World paulsin! ghgf hjhgjh'
 
 @app.route('/about')
 def about():
